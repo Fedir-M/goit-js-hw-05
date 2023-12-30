4caaa409baae926ee243dda1f354af49007a39dd
Normal One <149543061+Fedir-M@users.noreply.github.com>
2023-12-29 19:14:47 -0500

    homework-005

--- js/task-1.js
+++ js/task-1.js
@@ -1,69 +1,51 @@
-console.log("Задача 1. Пакування товарів")
+console.log("Задача 1. Імена користувачів")
 console.log("===========================")
 
-//TODO Напиши функцію isEnoughCapacity(products, containerSize), 
-//TODO яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
+//TODO Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об’єктів користувачів.
+//TODO Функція має повертати масив імен усіх користувачів (властивість name) із масиву users.
 
-//TODO Функція оголошує два параметри:
+const getUserNames = (users) => users.map((user)=> user.name)
 
-//TODO products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. 
-//TODO Наприклад, { apples: 2, grapes: 4 }.
-//TODO containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
 
-//TODO Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. 
-//TODO Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, 
-//TODO якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
-function isEnoughCapacity(products, containerSize) {
-    //! ========================================== variant 1 (doesn't work)
-//     let totalQnty = 0;
-
-//     for (const product in products) {
-//         totalQnty += products[product];
-
-//         if(totalQnty <= containerSize){
-//             return true;
-//       }
-//       return false;
-//     }
-// }
-// ========================================== variant 2 (work)
-let totalQnty = 0;
-
-    for (const product in products) {
-        totalQnty += products[product];
-    }
-
-    if(totalQnty <= containerSize){
-        return true;
-  }
-  return false;
-}
-
-// ========================================== variant 3 (work)
-// let totalQnty = 0;
-// for (const product in products) {
-//     totalQnty += products[product];
-//   }
-
-//   return totalQnty <= containerSize ? true : false;
-// }
-
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
-// У консоль будуть виведені результати її роботи.
-
+//* Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+//* У консоль будуть виведені результати її роботи.
 console.log(
-    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-  ); // false
-  
-  console.log(
-    isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-  ); // false
+  getUserNames([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    balance: 2764
+  },
+])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
